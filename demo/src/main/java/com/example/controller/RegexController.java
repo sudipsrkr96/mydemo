902cff53b1edb49e4a33586234ddd2e7fc5fad8f
sudipsrkr96 <108977563+sudipsrkr96@users.noreply.github.com>
2023-08-25 16:43:09 +0530

    Update RegexController.java

--- demo/src/main/java/com/example/controller/RegexController.java
+++ demo/src/main/java/com/example/controller/RegexController.java
@@ -17,8 +17,7 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/api/v1")
 public class RegexController {
-hello
-    hello new world
+ new world
     @Autowired
     private RegularExpressionService regularExpressionService;
 
